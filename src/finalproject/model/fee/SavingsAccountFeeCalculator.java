7db3908a9063eaab39a427f787ed1876f68793db
Eloiza <eloiza.souza@zup.com.br>
2025-01-08 16:30:40 -0300

    feat: add class SavingsAccountFeeCalculator

--- src/finalproject/model/fee/SavingsAccountFeeCalculator.java
+++ src/finalproject/model/fee/SavingsAccountFeeCalculator.java
@@ -1,4 +1,8 @@
 package finalproject.model.fee;
 
-public class SavingsAccountFeeCalculator {
+public class SavingsAccountFeeCalculator implements FeeCalculator {
+    @Override
+    public double calculateFee(double amount) {
+        return 0.0; // Sem taxa
+    }
 }
