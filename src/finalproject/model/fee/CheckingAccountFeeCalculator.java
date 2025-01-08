1c545bd075cc0981deccfebc379f305a6e4ad49f
Eloiza <eloiza.souza@zup.com.br>
2025-01-08 16:30:12 -0300

    feat: add class CheckingAccountFeeCalculator

--- src/finalproject/model/fee/CheckingAccountFeeCalculator.java
+++ src/finalproject/model/fee/CheckingAccountFeeCalculator.java
@@ -1,4 +1,9 @@
 package finalproject.model.fee;
 
-public class CheckingAccountFeeCalculator {
+public class CheckingAccountFeeCalculator implements FeeCalculator {
+    @Override
+    public double calculateFee(double amount) {
+        return amount * 0.01;
+    }
 }
+
