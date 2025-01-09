3fb56f6747632dec489779c5aa70936f673b2f6d
Eloiza <eloiza.souza@zup.com.br>
2025-01-09 15:11:51 -0300

    refactor: adapt AccountService

--- src/finalproject/service/AccountService.java
+++ src/finalproject/service/AccountService.java
@@ -1,60 +1,51 @@
 package finalproject.service;
 
 import finalproject.model.account.Account;
-import finalproject.model.notification.Notification;
 
 public class AccountService {
-    private final Account account;
-    private final Notification notification;
 
-    public AccountService(Account account, Notification notification) {
-        this.account = account;
-        this.notification = notification;
+    public double getBalance(Account account) {
+        return account.getBalance();
     }
 
-    public String getAccountNumber() {
-        return account.getAccountNumber();
-    }
-
-    public void deposit(double amount) {
-        validateAmount(amount);
+    public void deposit(Account account, double amount) {
+        validatePositiveAmount(amount);
         account.deposit(amount);
-        notification.sendNotification("Depósito efetuado no valor: " + amount);
     }
 
-    public void withdraw(double amount) {
-        validateAmount(amount);
-        if (amount > account.getBalance()) {
-            throw new IllegalArgumentException("Saldo insuficiente.");
-        }
+    public void withdraw(Account account, double amount) {
+        validatePositiveAmount(amount);
+        validateBalance(account, amount);
         account.withdraw(amount);
-        notification.sendNotification("Saque realizado no valor: " + amount);
     }
 
-    public void transfer(AccountService targetAccountService, double amount) {
-        if (targetAccountService == null) {
-            throw new IllegalArgumentException("Conta de destino inválida.");
-        }
-        validateAmount(amount);
-        account.withdraw(amount);
-        targetAccountService.account.deposit(amount);
-        targetAccountService.notification.sendNotification("Transferência recebida da conta " + getAccountNumber() + " no valor de " + amount);
-        notification.sendNotification("Transferência enviada para a conta " + targetAccountService.getAccountNumber() + " no valor de " + amount);
+    public void transfer(Account sourceAccount, Account targetAccount, double amount) {
+        validateAccount(sourceAccount);
+        validateAccount(targetAccount);
+        validatePositiveAmount(amount);
+        sourceAccount.withdraw(amount);
+        targetAccount.deposit(amount);
     }
 
-    public void applyRateInterest() {
+    public void applyRateInterest(Account account) {
         account.applyInterest();
-        notification.sendNotification("Rendimento aplicado com sucesso.");
     }
 
-    private void validateAmount(double amount) {
+    private void validatePositiveAmount(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("O valor deve ser maior que zero.");
         }
     }
 
-    public double getBalance() {
-        return account.getBalance();
+    private void validateAccount(Account account) {
+        if (account == null) {
+            throw new IllegalArgumentException("Conta inválida.");
+        }
     }
 
+    private void validateBalance(Account account, double amount) {
+        if (amount > account.getBalance()) {
+            throw new IllegalArgumentException("Saldo insuficiente.");
+        }
+    }
 }
