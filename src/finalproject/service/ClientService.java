b6293a2955c531bb5a95ccfa19868839fd24fe82
Eloiza <eloiza.souza@zup.com.br>
2025-01-09 15:12:27 -0300

    feat: implemetns ClientService class

--- src/finalproject/service/ClientService.java
+++ src/finalproject/service/ClientService.java
@@ -1,4 +1,56 @@
 package finalproject.service;
 
+import finalproject.model.account.Account;
+import finalproject.model.client.Client;
+import finalproject.model.notification.Notification;
+import finalproject.util.CpfValidator;
+import finalproject.util.EmailValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class ClientService {
+    private final List<Client> clients;
+
+    public ClientService() {
+        this.clients = new ArrayList<>();
+    }
+
+    public void addClient(String cpf, String name, String email, String cellPhoneNumber, Notification notification) {
+        validateCpf(cpf);
+        validateEmail(email);
+
+        Client client = new Client(cpf, name, email, cellPhoneNumber, notification);
+
+        clients.add(client);
+    }
+
+    private void validateEmail(String email) {
+        EmailValidator.validateEmail(email);
+    }
+
+    private void validateCpf(String cpf) {
+        CpfValidator.validateCpf(cpf);
+    }
+
+    public void addAccountToClient(String cpf, Account account) {
+        Client client = findClientByCpf(cpf)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado."));
+        client.addAccount(account);
+    }
+
+    public Optional<Client> findClientByCpf(String cpf) {
+        return clients.stream()
+                .filter(client -> client.getCpf().equals(cpf))
+                .findFirst();
+    }
+
+    public Optional<Account> findAccountByNumber(Client client, String accountNumber) {
+        return client.getAccounts().stream()
+                .filter(account -> account.getAccountNumber().equals(accountNumber))
+                .findFirst();
+    }
+
 }
+
