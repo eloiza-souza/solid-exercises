7fa3619869929d3c8a58f5fcd7092b89a60e5161
Eloiza <eloiza.souza@zup.com.br>
2025-01-09 10:10:15 -0300

    feat: implementes AccountService class methods

--- src/finalproject/service/AccountService.java
+++ src/finalproject/service/AccountService.java
@@ -1,4 +1,59 @@
 package finalproject.service;
 
+import finalproject.model.account.Account;
+import finalproject.model.notification.Notification;
+
 public class AccountService {
+    private final Account account;
+    private final Notification notification;
+
+    public AccountService(Account account, Notification notification) {
+        this.account = account;
+        this.notification = notification;
+    }
+
+    public String getAccountNumber() {
+        return account.getAccountNumber();
+    }
+
+    public void deposit(double amount) {
+        validateAmount(amount);
+        account.deposit(amount);
+        notification.sendMessage("Depósito efetuado no valor: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        validateAmount(amount);
+        if (amount > account.getBalance()) {
+            throw new IllegalArgumentException("Saldo insuficiente.");
+        }
+        account.withdraw(amount);
+        notification.sendMessage("Saque realizado no valor: " + amount);
+    }
+
+    public void transfer(Account targetAccount, double amount) {
+        if (targetAccount == null) {
+            throw new IllegalArgumentException("Conta de destino inválida.");
+        }
+        validateAmount(amount);
+        account.withdraw(amount);
+        targetAccount.deposit(amount);
+        notification.sendMessage("Transferência para a conta " + targetAccount.getAccountNumber() + " no valor de " + amount);
+    }
+
+    public void applyRateInterest() {
+        account.applyInterest();
+        notification.sendMessage("Rendimento aplicado com sucesso.");
+    }
+
+    private void validateAmount(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("O valor deve ser maior que zero.");
+        }
+    }
+
+    public double getBalance() {
+        return account.getBalance();
+    }
+
 }
